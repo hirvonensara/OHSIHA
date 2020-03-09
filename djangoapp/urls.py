5f364f120caf43725cb11124b5a0579422b15a06
Sara <sara.m.hirvonen@gmail.com>
2020-03-09 17:02:00 +0200

    Kirjautuminen

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+#from django.views.generic.base import TemplateView
 
 urlpatterns =[
     path('', views.HomePageView, name="kotisivu")
+    #path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    #path('signup/', views.SignUp.as_view(), name='signup'),
 ]
