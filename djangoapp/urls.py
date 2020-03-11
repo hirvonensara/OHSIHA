87c42735bfb5a9a6853a4407603faf1f68546116
Sara <sara.m.hirvonen@gmail.com>
2020-03-11 20:47:36 +0200

    Esimerkkidatan näyttäminen

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -3,8 +3,6 @@ from . import views
 from django.views.generic.base import TemplateView
 
 urlpatterns =[
-    #path('', views.HomePageView, name="kotisivu")
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    # path('signup/', views.SignUp.as_view(), name='signup'),
     path('example/', views.example, name='example')
 ]
--- djangoapp/views.py
+++ djangoapp/views.py
@@ -4,9 +4,33 @@ import requests
 # Create your views here.
 
 def example(request):
-   # response = requests.get('http://freegeoip.net/json/')
-   # geodata = response.json()
+    count_in_pirkanmaa = 0
+    count_in_hus = 0
+    count_in_other = 0
+    all_cases = 0
+
+    response = requests.get('https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/')
+    coronadata = response.json()
+    list_of_confirmed = coronadata['confirmed']
+
+    for case in list_of_confirmed:
+        
+        if case['healthCareDistrict'] == 'Pirkanmaa':
+            count_in_pirkanmaa += 1
+        
+        elif case['healthCareDistrict'] == 'HUS' :
+            count_in_hus += 1
+        
+        else:
+            count_in_other += 1
+
+    all_cases = count_in_hus + count_in_pirkanmaa + count_in_other        
+
     return render(request, 'index.html', {
-      #  'ip': geodata['ip'],
-      #  'country': geodata['country_name']
+      'count_pirkanmaa' : count_in_pirkanmaa,
+      'count_hus' : count_in_hus,
+      'count_other' : count_in_other,
+      'all' : all_cases
+
+      
     })
