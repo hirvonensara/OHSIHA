859905c87f0b35a14d5a1ff930bf0529f5964bc1
Sara <sara.m.hirvonen@gmail.com>
2020-03-31 16:35:45 +0300

    Päivitetty home-näkymä esittämään dataa

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -4,8 +4,41 @@ import requests
 # Create your views here.
 
 def home(request):
+    # Health care districts are given an index number
+    healthcaredistricts = {'null': 0, 'HUS': 1, 'Pirkanmaa':2, 'Lappi': 3,
+                            'Kanta-Häme' : 4, 'Etelä-Karjala' : 5, 'Pohjois-Pohjanmaa': 6,
+                            'Pohjois-Savo':7, 'Varsinais-Suomi':8, 'Keski-Suomi':9,
+                            'Pohjois-Karjala':10, 'Etelä-Pohjanmaa': 11, 'Satakunta':12,
+                            'Vaasa':13, 'Keski-Pohjanmaa':14, 'Päijät-Häme':15, 'Etelä-Savo':16,
+                            'Kymenlaakso':17, 'Länsi-Pohja':18, 'Ahvenanmaa':19, 'Itä-Savo':20, 'Kainuu':21}
 
-    return render(request, 'home.html' )
+    # Creates a list for confirmed cases which is as big 
+    confirmed_cases = len(healthcaredistricts)*[0]
+
+    # Gets data 
+    response = requests.get('https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/')
+    coronadata = response.json()
+    list_of_confirmed = coronadata['confirmed']
+
+    for case in list_of_confirmed:
+
+        if case['healthCareDistrict'] in healthcaredistricts.keys():
+            index = healthcaredistricts[case['healthCareDistrict']]
+            confirmed_cases[index] = confirmed_cases[index] + 1
+
+        else:
+            confirmed_cases[0] = confirmed_cases[0] + 1
+
+    # Counts all the cases together
+    all_cases = 0
+    for number in confirmed_cases:
+        all_cases += number
+
+    return render(request, 'home.html',
+    {
+        'districts' : healthcaredistricts,
+        'confirmed' : confirmed_cases
+    } )
 
 def example(request):
     count_in_pirkanmaa = 0
