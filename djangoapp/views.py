380266149a23429653aff69c69d568c9c714ffba
Sara <sara.m.hirvonen@gmail.com>
2020-04-01 15:30:40 +0300

    Lisätty recovered ja deaths

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -12,40 +12,78 @@ def home(request):
                             'Vaasa':13, 'Keski-Pohjanmaa':14, 'Päijät-Häme':15, 'Etelä-Savo':16,
                             'Kymenlaakso':17, 'Länsi-Pohja':18, 'Ahvenanmaa':19, 'Itä-Savo':20, 'Kainuu':21}
 
-    # Creates a list for confirmed cases which is as big 
+    # Creates lists for confirmed, deaths and recovered 
     confirmed_cases = len(healthcaredistricts)*[0]
+    deaths = len(healthcaredistricts)*[0]
+    recovered = len(healthcaredistricts)*[0]
 
     # Gets data 
     response = requests.get('https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/')
     coronadata = response.json()
     list_of_confirmed = coronadata['confirmed']
+    list_of_deaths = coronadata['deaths']
+    list_of_recovered = coronadata['recovered']
 
+    # Goes through confirmed cases and sorts them by health care district
+    # and counts them all together
+    all_cases = 0
     for case in list_of_confirmed:
 
         if case['healthCareDistrict'] in healthcaredistricts.keys():
             index = healthcaredistricts[case['healthCareDistrict']]
             confirmed_cases[index] = confirmed_cases[index] + 1
+            all_cases += 1
 
         else:
             confirmed_cases[0] = confirmed_cases[0] + 1
+            all_cases += 1
 
-    # Counts all the cases together
-    all_cases = 0
-    for number in confirmed_cases:
-        all_cases += number
+    # Goes through deaths and sorts them by health care district
+    # and counts them all together
+    all_deaths = 0
+    for d in list_of_deaths:
 
-    confirmed_dct = {}
+        if d['healthCareDistrict'] in healthcaredistricts.keys():
+            index = healthcaredistricts[d['healthCareDistrict']]
+            deaths[index] = deaths[index] + 1
+            all_deaths += 1
 
-    for i in range(0, 22):
+        # if health care district is unknown (null)
+        else:
+            deaths[0] = deaths[0] + 1
+            all_deaths += 1
+
+    # Goes through recovered and sorts them by health care district
+    # and counts them all together
+    all_recovered = 0
+    for r in list_of_recovered:
+
+        if r['healthCareDistrict'] in healthcaredistricts.keys():
+            index = healthcaredistricts[r['healthCareDistrict']]
+            recovered[index] = recovered[index] + 1
+            all_recovered += 1
+
+        # if health care district is unknown (null)
+        else:
+            recovered[0] = recovered[0] + 1
+            all_recovered += 1
+    
+
+    corona_dct = {}
+    data_lst = []
+
+    for i in range(0, len(healthcaredistricts)):
         districts = list(healthcaredistricts.keys())
         district = districts[i]
-        confirmed = confirmed_cases[i]
-        confirmed_dct[district] = confirmed
+        data_lst = [ confirmed_cases[i], deaths[i], recovered[i] ]
+        corona_dct[district] = data_lst
 
     return render(request, 'home.html',
     {
-        'confirmed' : confirmed_dct,
-        'all_confirmed' : all_cases
+        'coronadata' : corona_dct,
+        'all_confirmed' : all_cases,
+        'all_deaths' : all_deaths,
+        'all_recovered' : all_recovered
     } )
 
 def example(request):
